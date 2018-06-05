978ffb578db28101d5c28db5a4eddc2fc0a924a8
Hendi Santika <hendisantika@gmail.com>
2018-06-06 05:34:06 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmongodbreact/SpringBootMongodbReactApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootmongodbreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbReactApplication.class, args);
+    }
+}
